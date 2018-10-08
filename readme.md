65b8c2c77a717147fe1ad9bbc49eb1bd67f16d15
DoniaKRAMTI <you@example.com>
2018-10-08 11:44:56 +0200

    Setup^production webpack build

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Git commands
+ 
+ git init - Create a ne git repo 
+ git status - view the changes to your project code 
+ git add - Add files to staging area 
+ git commit - creates a new commit with files from staging area 
+ git log - view recent commits
+ 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
+module.exports = (env) => {
+  const isProduction = env ==='production';
+  console.log ('env' , env);
+  return {
 
-module.exports = {
   entry: './src/app.js',
   output: {
     path: path.join(__dirname, 'public'),
@@ -20,9 +23,13 @@ module.exports = {
       ]
     }]
   },
-  devtool: 'cheap-module-eval-source-map',
+  devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     historyApiFallback: true
+
   }
+}
+
+ 
 };
